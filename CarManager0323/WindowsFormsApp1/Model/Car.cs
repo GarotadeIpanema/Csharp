6486be8ab35b083f51bc73d6232c04373eb468a4
GarotadeIpanema <75405577+GarotadeIpanema@users.noreply.github.com>
2021-03-23 16:49:14 +0900

    씨샵 DB 연결_차량관리 프로그램_0323
    -차량 관리 프로그램 만들기

---
+++ CarManager0323/WindowsFormsApp1/Model/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    class Car
+    {
+        string model;
+        int price;
+        string color;
+        string company;
+        string year;
+
+        public Car(string model, int price, string color, string company, string year)
+        {
+            this.model = model;
+            this.price = price;
+            this.color = color;
+            this.company = company;
+            this.year = year;
+        }
+    }
+
+
+}
---
+++ CarManager0323/WindowsFormsApp1/Model/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    class Customer
+    {
+        string name;
+        string tel;
+        string email;
+        string address;
+
+        public Customer(string name, string tel, string email, string address)
+        {
+            this.name = name;
+            this.tel = tel;
+            this.email = email;
+            this.address = address;
+        }
+
+
+    }
+}
