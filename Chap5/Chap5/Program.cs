0776ded5d200c87194794f792b6f82da143da412
GarotadeIpanema <75405577+GarotadeIpanema@users.noreply.github.com>
2021-04-08 16:41:19 +0900

    로또번호생성기

---
+++ Chap5/Chap5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chap5
+{
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+       
+        public void SetIntime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public void SetOuttime()
+        {
+            this.outTime = DateTime.Now;
+        }
+
+        static void Main(string[] args)
+        {
+            //클래스와 윈폼 듀듕
+            //클래스: 하나의 변수에 여러개의 변수를 저장할 수 있다
+            Random random = new Random();
+            Console.WriteLine( random.Next(10,100));
+            Console.WriteLine(random.Next(10,100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+            Car car = new Car();
+            car.SetIntime();
+            car.SetOuttime();
+            //클래스, 인스턴스 ,new, 생성자
+
+
+        }
+    }
+}
