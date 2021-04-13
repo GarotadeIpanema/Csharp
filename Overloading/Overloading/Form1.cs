0617adbcf286fd99ae8a47db89374ec189451490
GarotadeIpanema <75405577+GarotadeIpanema@users.noreply.github.com>
2021-04-13 16:43:36 +0900

    오버로딩 개념 복습, 윈폼 야구게임 수정
    -야구게임 타이머기능 추가

---
+++ Overloading/Overloading/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Overloading
+{
+    public partial class Form1 : Form
+    {   //재사용=오버로딩
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void black()
+        {
+            MessageBox.Show("커피");
+        }
+        private void black(string name)
+        {
+            MessageBox.Show(name);
+        }
+        private void black(string name, string name2)
+        {
+            MessageBox.Show(name+name2);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            black(button2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            black();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            black(button2.Text, button1.Text);
+        }
+    }
+}
