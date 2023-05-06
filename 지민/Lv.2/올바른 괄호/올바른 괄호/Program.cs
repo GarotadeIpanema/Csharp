1f0a711c58712228710345c273ad8313e9b4e7d1
GarotadeIpanema <75405577+GarotadeIpanema@users.noreply.github.com>
2023-05-06 20:56:04 +0900

    32434

---
+++ 지민/Lv.2/올바른 괄호/올바른 괄호/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System.ComponentModel.Design;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+namespace Sample
+{
+    class Sample
+    {
+        static void Main()
+        {
+            //올바른 괄호
+            string s = ")()(";
+            var chars = s.ToCharArray();
+            int open = 0; bool answer = false;
+            if (s[0] != ')')
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    // i= Array.IndexOf(chars,"(");
+                    i = Array.IndexOf(chars, '(');
+                    if (chars[i] == '(')
+                    {
+                        open++;
+                    }
+                    else
+                    {
+                        open--;
+                    }
+                    if (open < 0)
+                    {
+                        answer = false;
+                    }
+                }
+            }
+
+            answer = open == 0 ? true : false;
+            Console.WriteLine(answer);
+
+        }
+    }
+}
---
+++ 지민/Lv.2/이진변환반복하기/이진변환반복하기/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count = 0; int le = 0;
+int[] answer = new int[] { 0, 0 };
+string s = "110010101001";
+while (s != "1")
+{
+    answer[1] += s.ToCharArray().Where(x => x == '0').Count();
+    s = s.Replace("0", "");
+    le = s.Length;
+    s = Convert.ToString(le, 2);
+    count++;
+}
+answer[0] = count;
+Console.WriteLine(String.Join(" ", answer));
